edc81f57846170f7b78d1331320d4d00d2dd6009
Hewan Gete <hewangete@gmail.com>
2025-11-24 18:57:55 +0400

    merge fix

--- nextjs-app/app/components/Layout.tsx
+++ nextjs-app/app/components/Layout.tsx
@@ -15,7 +15,6 @@ import Image from "next/image"
 
 import { Button } from "./ui";
 import React from "react";
-import Image from "next/image";
 
 interface LayoutProps {
   children: React.ReactNode;
